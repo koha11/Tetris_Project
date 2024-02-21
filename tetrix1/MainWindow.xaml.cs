477cc6e95f45531f18130ff9b48c0b37d644533c
= <=>
2024-02-21 14:17:43 +0700

    done

--- tetrix1/MainWindow.xaml.cs
+++ tetrix1/MainWindow.xaml.cs
@@ -19,6 +19,109 @@ namespace tetrix1
         public MainWindow()
         {
             InitializeComponent();
+            imageControls = SetupGameCanvas(gameState.GameGrid);
         }
+
+        private Image[,] SetupGameCanvas(GameGrid grid)
+        {
+            Image[,] imageControls = new Image[grid.Rows, grid.columns];
+            int cellSize = 25;
+
+            for(int r = 0; r < grid.Rows; r++)
+            {
+                for(int c = 0; c < grid.Columns; c++)
+                {
+                    Image imageControl = new Image
+                    {
+                        Width = cellSize;
+                        Height = cellSize;
+                    };
+
+                    Canvas.SetTop(imageControl, (r-2)*cellSize);
+                    Canvas.SetLeft(imageControl, c*cellSize);
+                    GameCanvas.Children.Add(imageControl);
+                    imageControls[r, c] = imageControl;
+                }
+            }
+            
+            return imageControls;
+        }
+
+        private void DrawGrid(GameGrid grid)
+        {
+            for (int r = 0; r < grid.Rows; r++)
+            {
+                for (int c = 0;  < grid.columns; c++)
+                {
+                    int id = grid[r, c];
+                    imageControls[r,c].Source = tileImages[id];
+                }
+            }
+        }
+
+        private void DrawBlock(Block block)
+        {
+            foreach (Position p in block.TilePositions())
+            {
+                imageControls[p.Row, p.Column].Source = tileImage[block.id];
+            }
+        }
+
+        private void Draw(GameState gameState)
+        {
+            DrawGrid(gameState.GameGrid);
+            DrawBlock(gameState.CurrentBlock);
+        }
+
+        private async Task GameLoop()
+        {
+            Draw(gameState);
+
+            while(!gameState.GameOver)
+            {
+                await Task.Delay(500);
+                gameState.MoveBlockDown();
+                Draw(gameState);
+            }
+        }
+
+        private void Window_keydown(object sender, KeyEventArgs e)
+        {
+            if(gameState.GameOver)
+            {
+                return;
+            }
+
+            switch(e.Key)
+            {
+                case Key.Left:
+                    gameState.MoveBlockLeft();
+                    break;
+                case Key.Right:
+                    gameState.MoveBlockRight();
+                    break;
+                case Key.Down:
+                    gameState.MoveBlockDown();
+                    break;
+                case Key.Up:
+                    gameState.RotateBlockCW();
+                    break;
+                case Key.Z:
+                    gameState.RotateBlockCWW();
+                    break;
+                default:
+                    return;
+            }
+
+            Draw(gameState);
+        }
+
+        private async void GameCanvas_Loaded(object sender, RouteEventArgs e)
+        {
+            await GameLoop();
+        }
+
+
     }
+
 }
