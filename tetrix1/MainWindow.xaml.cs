53f8a1d8df1a2d1fc9a84ee194d7c521934f940d
= <=>
2024-02-26 20:22:20 +0700

    Fix loi chinh ta

--- tetrix1/MainWindow.xaml.cs
+++ tetrix1/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace tetrix1
+namespace tetris1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
--- tetrix1/obj/Debug/net8.0-windows/tetris1.AssemblyInfo.cs
+++ tetrix1/obj/Debug/net8.0-windows/tetris1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("tetrix1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("tetris1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8def771056a80f36abbac531a244078a4ad10570")]
-[assembly: System.Reflection.AssemblyProductAttribute("tetrix1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("tetrix1")]
+[assembly: System.Reflection.AssemblyProductAttribute("tetris1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("tetris1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
