3aacc05ed922a8071a1cd1b616f8004096dfa2ad
Nguyen Duc Trung <nguyenductrung20041002@gmail.com>
2024-02-27 11:28:37 +0700

    add file

---
+++ tetris1/Position.cs
@@ -0,0 +1,17 @@
+﻿namespace Tetris
+{
+    public class Position
+    {
+        public int Row {  get; set; }
+
+        public int Column { get; set; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+    
+}
+
