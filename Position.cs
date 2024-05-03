34a1b3502181fa890910f7f3c32f5a002115c5a2
HaNguyenMinhTam <127753137+HaNguyenMinhTam@users.noreply.github.com>
2024-05-03 09:47:19 +0700

    Update Position.cs

--- Position.cs
+++ Position.cs
@@ -1,14 +1,20 @@
 ﻿namespace Tetris1
 {
+     //Khai báo lớp Position
     public class Position
     {
+        //Khai báo thuộc tính Row để lưu trữ hàng của 1 ô trong lưới game
         public int Row {  get; set; }
 
+        //Khai báo thuộc tính Column để lưu trữ cột của 1 ô trong lưới game
         public int Column { get; set; }
 
+        //Phương thức khởi tạo cho lớp Position với hai tham số là hàng và cột
         public Position(int row, int column)
         {
+            //Gán giá trị cho thuộc tính Row
             Row = row;
+            //Gán giá trị cho thuộc tính Column
             Column = column;
         }
     }
