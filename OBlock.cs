187d3b251b1ff1e5a4854a6c18f3b2d1609ddba3
Nguyễn Đức Trung <117899692+ductrunkk@users.noreply.github.com>
2024-05-03 19:58:06 +0700

    Add Comment OBlock.cs

--- OBlock.cs
+++ OBlock.cs
@@ -1,14 +1,20 @@
 ﻿namespace Tetris1
 {
-    public class OBlock : Block
+    public class OBlock : Block //Lớp con khối O
     {
+        //Khối O chiếm cùng một vị trí trong mọi trạng thái xoay
         private readonly Position[][] tiles = new Position[][]
         {
             new Position[] { new(0,0), new(0,1), new(1,0), new(1,1) }
         };
+        //Các thuộc tính của block O:
+        //Mã số block O là 4
         public override int Id => 4;
-
+        
+         //Offset bắt đầu có tọa độ là (0,4) để khối xuất hiện ở giữa hàng trên cùng
         protected override Position StartOffset => new Position(0,4);
+        
+        //Trả về mảng vị trí ô ở trên
         protected override Position[][] Tiles => tiles;
     }
 }
