9b56113c1994f2e5061c3366db6d36ca774194b5
Nguyễn Đức Trung <117899692+ductrunkk@users.noreply.github.com>
2024-05-03 21:23:08 +0700

    Add Comment SBlock.cs

--- SBlock.cs
+++ SBlock.cs
@@ -1,7 +1,8 @@
 ﻿namespace Tetris1
 {
-    public class SBlock : Block
+    public class SBlock : Block //Lớp con khối S
     {
+        //Lưu trữ các vị trí ô cho bốn trạng thái xoay 
         private readonly Position[][] tiles = new Position[][]
         {
             new Position[] { new (0,1), new (0,2), new (1,0), new (1,1) },
@@ -9,8 +10,14 @@
             new Position[] { new (1,1), new (1,2), new (2,0), new (2,1) },
             new Position[] { new (0,0), new (1,0), new (1,1), new (2,1) },
         };
+        //Các thuộc tính của block S:
+        // mã số block S là 5
         public override int Id => 5;
+
+        //Offset bắt đầu có tọa độ là (0, 3) để khối xuất hiện ở giữa hàng trên cùng
         protected override Position StartOffset => new Position(0, 3);
+
+        //Trả về mảng vị trí ô ở trên
         protected override Position[][] Tiles => tiles;
 
     }
